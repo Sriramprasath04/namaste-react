4ac291b7958c19a733808dd4d5d352fe7610080c
Sriramprasath <sriramprasath04@gmail.com>
2024-02-28 13:08:32 +0530

    l3

--- l3/App.js
+++ l3/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const Component1 = () => (
+    <h1 className = "head">This is Component-1 </h1>
+) 
 
-const parent = React.createElement("div", {id:"parent"}, 
-    React.createElement("div", {id:"child"}, [
-        React.createElement("h1", {}, "This is Namaste React🥇"),
-        React.createElement("h2", {}, "This is h2 tag"),
-    ])
+const Component2 = () => (
+    <div className = "container">
+        <Component1></Component1>
+        <Component1/>
+        {Component1()}
+        <h1>Inside a React Component-2</h1>
+    </div>
+    
 );
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Component2/>);
+
